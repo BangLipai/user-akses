8f3a5c1472012540900ff8f506258824239b5d6b
BangLipai <developer.kukuh@gmail.com>
2024-01-27 14:57:29 +0700

    Akses service

---
+++ src/Services/AksesService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BangLipai\UserAkses\Services;
+
+use App\Models\User;
+use BangLipai\UserAkses\Models\UserAkses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+
+class AksesService
+{
+    public function fromRequest(Request $request): string
+    {
+        $controller = $request->route()->getActionName();
+        return $this->fromController($controller);
+    }
+
+    public function isAkses(Collection $userAkses, int $kAkses): bool
+    {
+        return $userAkses->contains($kAkses);
+    }
+
+    public function getUserAkses(User $user, Collection $groupAkses): Collection
+    {
+        $userAkses = UserAkses::query()
+            ->where('id', '=', $user->id)
+            ->get();
+
+        $kAkseses = $userAkses->pluck('k_akses');
+        return $kAkseses->merge($groupAkses->pluck('k_akses'))->unique();
+    }
+
+    /**
+     * @param $controller
+     *
+     * @return string
+     */
+    protected function fromController($controller): string
+    {
+        $controller = Str::replace('App\\Http\\Controllers\\', '', $controller);
+        $controller = Str::replace('Controller@', '.', $controller);
+        $controller = Str::replace('\\', '', $controller);
+
+        return Str::lower(Str::snake($controller, '-'));
+    }
+}
